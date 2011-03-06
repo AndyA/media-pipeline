0e6618f0ea72024411d8b13799210811fad2b2d9
Andy Armstrong <andy@hexten.net>
2011-03-06 13:43:01 +0000

    Initial checkin.

---
+++ lib/Media/Pipeline.pm
@@ -0,0 +1,44 @@
+package Media::Pipeline;
+
+use warnings;
+use strict;
+
+use Carp qw( croak );
+use Path::Class;
+
+our $VERSION = '0.04';
+
+=head1 NAME
+
+Media::Pipeline - Media processing pipelines
+
+=head1 VERSION
+
+This document describes Media::Pipeline version 0.04
+
+=head1 SYNOPSIS
+
+  use Media::Pipeline;
+
+=head1 DESCRIPTION
+
+=head1 INTERFACE
+
+=cut
+
+1;
+__END__
+
+=head1 DEPENDENCIES
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2011, Andy Armstrong C<< <andy@hexten.net> >>. All
+rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
---
+++ t/00-load.t
@@ -0,0 +1,8 @@
+use Test::More tests => 1;
+
+BEGIN {
+  use_ok( 'Media::Pipeline' );
+}
+
+diag(
+  "Testing Media::Pipeline $Media::Pipeline::VERSION" );
